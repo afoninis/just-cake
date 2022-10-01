d6eb5052c73cecf043aa3d8be0d9ec98b83f08ab
afoninis <afoninartofficial@gmail.com>
2022-10-01 18:59:32 +0300

    Done: Header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-
+const swiperHeader = new Swiper(".header__swiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
